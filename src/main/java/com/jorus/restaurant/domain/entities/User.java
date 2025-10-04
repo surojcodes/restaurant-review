742086bc177d042784cc426c9be374027ee60425
suroj <surojmaharjan0@gmail.com>
2025-10-04 14:09:48 -0700

    Basic setup and Entity creation

---
+++ src/main/java/com/jorus/restaurant/domain/entities/User.java
@@ -0,0 +1,26 @@
+package com.jorus.restaurant.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Field(type = FieldType.Keyword) // Keyword for exact matches
+    private String id;
+
+    @Field(type = FieldType.Text) // Text for full-text search/partial matches
+    private String username;
+
+    @Field(type = FieldType.Text)
+    private String givenName;
+
+    @Field(type = FieldType.Text)
+    private String familyName;
+}
